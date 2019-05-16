7f6f0666e98bf3e2597de094deb1c429f97f78a9
Kat Maldon <katharinamaldon@gmail.com>
2019-05-16 15:46:03 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,26 +1,26 @@
 def highest_student_gpa
   "SELECT MAX(gpa) FROM students;"
-end	end
+end
 
 
- def lowest_student_gpa	def lowest_student_gpa
+ def lowest_student_gpa	
   "SELECT MIN (students.gpa) FROM students;"
-end	end
+end
 
 
-def average_student_gpa	def average_student_gpa
+def average_student_gpa	
   "SELECT AVG(students.gpa) FROM students;"
-end	end
+end
 
 
-def total_tardies_for_all_students	def total_tardies_for_all_students
+def total_tardies_for_all_students	
   "SELECT SUM(students.tardies) FROM students;"
-end	end
+end
 
 
- def counts_all_students_groups_by_grade	def counts_all_students_groups_by_grade
+def counts_all_students_groups_by_grade	
   "SELECT COUNT(*) FROM students GROUP BY grade;"
-end	end
+end	
 
 
 def average_student_gpa_for_9th_grade
